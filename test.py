1b42e0eff7e10ac9169fe4f940d9032249834031
76665376 <103045274+76665376@users.noreply.github.com>
2022-04-05 07:52:15 -0500

    mi primer archivo python
    lo que hace es imprimir en la pantalla

---
+++ test.py
@@ -0,0 +1,12 @@
+#variables de tipo cadena
+nombre = "Enrique"
+apellido = 'Aguilar'
+nombreapellido = nombre + ' ' + apellido
+
+#imprimir cadenas
+print(nombre)
+print(apellido)
+print (nombreapellido)
+
+#imprimir tipos
+print(type(nombre))
